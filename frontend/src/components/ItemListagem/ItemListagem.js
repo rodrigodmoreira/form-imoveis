6172c7e44b54f5a634a48b0a54e6a731d8252f07
Antonio Sousa <antonioaads@gmail.com>
2019-10-30 11:01:25 -0300

    Algumas mudanças de layout e de funcionalidade no componente item

--- frontend/src/components/ItemListagem/ItemListagem.js
+++ frontend/src/components/ItemListagem/ItemListagem.js
@@ -1,46 +1,71 @@
 import React from 'react'
 
-import { FaTrash, FaEdit, FaUserEdit} from 'react-icons/fa';
+import { FaTrash, FaEdit, FaBed, FaRulerHorizontal} from 'react-icons/fa';
 
 import styles from './ItemListagem.module.css'
 
 
-const ItemListagem = (props) => {
-    return (
-        <div className={styles["container"]}>
-            <div className={styles["header"]}>
-                <div className={styles["tipo"]}>
-                    {props.tipo}
+class ItemListagem extends React.Component {
+    render(){
+        return (
+            <div className={styles["container"]}>
+                <div className={styles["header"]}>
+                    <div className={styles["tipo"]}>
+                        {this.props.tipo}
+                    </div>
+                    <div className={styles["rua"]}>
+                        {this.props.rua}
+                    </div>
+                    <div className={styles["bairro"]}>
+                        {this.props.bairro}
+                    </div>
+    
+                    <div className={styles["informacoes"]}>
+                        <div className={styles["info-detalhe"]}>
+                            <div className={styles["icones"]}>
+                                <FaRulerHorizontal />
+                            </div>
+                            <div>
+                            {this.props.metrosQuadrados}
+                            </div>
+                        </div>
+                        <div className={styles["info-detalhe"]}>
+                            <div className={styles["icones"]}>
+                                <FaBed />
+                            </div>
+                            <div>
+                            {this.props.numeroQuartos}
+    
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
-                <div className={styles["rua"]}>
-                    {props.rua}
-                </div>
-                <div className={styles["bairro"]}>
-                    {props.bairro}
-                </div>
-
-                <div className={styles["informacoes"]}>
-                    {props.metrosQuadrados}
-                    {props.numeroQuartos}
+                <div className={styles["footer"]}>
+                    <div className={styles["precos"]}>
+                        Preco x
+                    </div>
+                    <div className={styles["iconesAcao"]}>
+                        <div className={styles["button"]} onClick={this.acao}>
+                            <FaTrash /> 
+                        </div>
+                        <div className={styles["button"]}>
+                            <FaEdit />
+                        </div>
+                    </div>
+    
+                    
                 </div>
             </div>
-            <div className={styles["footer"]}>
-                <div className={styles["precos"]}>
-                    Preco x
-                </div>
-                <div className={styles["iconesAcao"]}>
-                    <div className={styles["button"]}>
-                        <FaTrash /> 
-                    </div>
-                    <div className={styles["button"]}>
-                        <FaEdit />
-                    </div>
-                </div>
+        );
+    }
 
-                
-            </div>
-        </div>
-    );
+    acao = () => {
+        console.log("teste2");
+    }
+    
 }
+
+    
 
 export default ItemListagem;
