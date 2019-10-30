f122b9e8aa01e05681e950dd914f5e1212e8059c
Antonio Sousa <antonioaads@gmail.com>
2019-10-30 10:27:08 -0300

    Personalização dos cards de aluguel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,43 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ItemListagem/>
+        <ItemListagem 
+          tipo="casa" 
+          rua="amazonas" 
+          bairro="nova suissa" 
+          metrosQuadrados="100" 
+          numeroQuartos="4"
+        />
+
+        <ItemListagem 
+          tipo="apartamento" 
+          rua="amazonas" 
+          bairro="nova suissa" 
+          metrosQuadrados="100" 
+          numeroQuartos="4"
+        />  
+        <ItemListagem 
+          tipo="apartamento" 
+          rua="amazonas" 
+          bairro="nova suissa" 
+          metrosQuadrados="100" 
+          numeroQuartos="4"
+        /> 
+        <ItemListagem 
+          tipo="apartamento" 
+          rua="amazonas" 
+          bairro="nova suissa" 
+          metrosQuadrados="100" 
+          numeroQuartos="4"
+        /> 
+        <ItemListagem 
+          tipo="apartamento" 
+          rua="amazonas" 
+          bairro="nova suissa" 
+          metrosQuadrados="100" 
+          numeroQuartos="4"
+        /> 
+        
       </header>
     </div>
   );
--- frontend/src/components/ItemListagem/ItemListagem.js
+++ frontend/src/components/ItemListagem/ItemListagem.js
@@ -1,19 +1,43 @@
 import React from 'react'
 
-import styles from './ItemListagem.module.css'
-import { IconContext } from "react-icons";
+import { FaTrash, FaEdit, FaUserEdit} from 'react-icons/fa';
 
-const ItemListagem = () => {
+import styles from './ItemListagem.module.css'
+
+
+const ItemListagem = (props) => {
     return (
         <div className={styles["container"]}>
             <div className={styles["header"]}>
-                Ola
+                <div className={styles["tipo"]}>
+                    {props.tipo}
+                </div>
+                <div className={styles["rua"]}>
+                    {props.rua}
+                </div>
+                <div className={styles["bairro"]}>
+                    {props.bairro}
+                </div>
+
+                <div className={styles["informacoes"]}>
+                    {props.metrosQuadrados}
+                    {props.numeroQuartos}
+                </div>
             </div>
             <div className={styles["footer"]}>
- 
-                <IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+                <div className={styles["precos"]}>
+                    Preco x
+                </div>
+                <div className={styles["iconesAcao"]}>
+                    <div className={styles["button"]}>
+                        <FaTrash /> 
+                    </div>
+                    <div className={styles["button"]}>
+                        <FaEdit />
+                    </div>
+                </div>
+
                 
-                </IconContext.Provider>
             </div>
         </div>
     );
