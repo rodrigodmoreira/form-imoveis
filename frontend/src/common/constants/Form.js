2098635cb03e1f3dad2bb154f9336fc260a8b736
Antonio Sousa <antonioaads@gmail.com>
2019-11-24 20:36:34 -0300

    [FRONT] Melhoria no controle de estado do form

---
+++ frontend/src/common/constants/Form.js
@@ -0,0 +1,14 @@
+const Form = {
+    Campos: {
+        quartos: "quartos",
+        suites: "suites",
+        estar: "estar",
+        jantar: "jantar",
+        area: "area",
+        vagas: "vagas",
+        andar: "andar",
+        condominio: "condominio",
+    }
+}
+
+export default Form;
--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import ConstantesForm from '../../common/constants/Form';
 import ConstantesImoveis from '../../common/constants/Imoveis';
 
 import styles from './Form.module.css';
@@ -8,19 +9,50 @@ class Form extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            checkIn: '',
-            checkOut: '',
             acao: props.acao,
             tipoImovel: ConstantesImoveis.Tipos.Apartamento,
+            quartos: null,
+            suites: null,
+            estar: null,
+            jantar: null,
+            area: null,
+            vagas: null,
+            andar: null,
+            condominio: null,
         };
     }
 
-    handleChangeCheckIn = (event) => {
-        this.setState({checkIn: event.target.value});
-    }
+    handleChange = (event) => {
+        const value = event.target.value;
 
-    handleChangeCheckOut = (event) => {
-        this.setState({checkOut: event.target.value});
+        switch(event.target.name){
+            case ConstantesForm.Campos.quartos:
+                this.setState({ quartos: value});
+                break;
+            case ConstantesForm.Campos.suites:
+                this.setState({ suites: value});
+                break;
+            case ConstantesForm.Campos.estar:
+                this.setState({ estar: value});
+                break;
+            case ConstantesForm.Campos.jantar:
+                this.setState({ jantar: value});
+                break;
+            case ConstantesForm.Campos.area:
+                this.setState({ area: value});
+                break;
+            case ConstantesForm.Campos.vagas:
+                this.setState({ vagas: value});
+                break;
+            case ConstantesForm.Campos.andar:
+                this.setState({ andar: value});
+                break;
+            case ConstantesForm.Campos.condominio:
+                this.setState({ condominio: value});
+                break;
+            default:
+                break;
+        }
     }
     
     handleSubmit = (event) => {
@@ -35,7 +67,7 @@ class Form extends React.Component{
     }
 
     render(){
-        const { tipoImovel } = this.state;
+        const { tipoImovel, quartos, suites, estar, jantar, area, vagas, andar, condominio } = this.state;
      
         return(
             <div className={styles["container"]}>
@@ -52,28 +84,36 @@ class Form extends React.Component{
                         <label>
                             <br></br>
                             <span className={styles["check"]}>Numero de Quartos:</span>
-                            <input type="number" placeholder="Numero de Quartos" value={this.state.checkIn} onChange={this.handleChangeCheckIn} />
+                            <input type="number" name={ConstantesForm.Campos.quartos} placeholder="Numero de Quartos" value={ quartos } onChange={this.handleChange} />
                         </label>
                         <label>
                             <span className={styles["check"]}>Número de Suítes:</span>
-                            <input type="number" placeholder="Número de Suítes" value={this.state.checkOut} onChange={this.handleChangeCheckOut} />
+                            <input type="number" name={ConstantesForm.Campos.suites} placeholder="Número de Suítes" value={ suites } onChange={this.handleChange} />
                         </label>
                         <label>
                             <span className={styles["check"]}>Salas de Estar:</span>
-                            <input type="number" placeholder="Número de Sala de Estar" value={this.state.checkOut} onChange={this.handleChangeCheckOut} />
+                            <input type="number" name={ConstantesForm.Campos.estar} placeholder="Número de Sala de Estar" value={ estar } onChange={this.handleChange} />
                         </label>
                         {tipoImovel === ConstantesImoveis.Tipos.Apartamento && <label>
                             <span className={styles["check"]}>Salas de Jantar:</span>
-                            <input type="number" placeholder="Número de Sala de Jantar" value={this.state.checkOut} onChange={this.handleChangeCheckOut} />
+                            <input type="number" name={ConstantesForm.Campos.jantar} placeholder="Número de Sala de Jantar" value={ jantar } onChange={this.handleChange} />
                         </label>}
                         <label>
                             <span className={styles["check"]}>Área em metros Quadrados:</span>
-                            <input type="number" placeholder="Área em m2" value={this.state.checkOut} onChange={this.handleChangeCheckOut} />
+                            <input type="number" name={ConstantesForm.Campos.area} placeholder="Área em m2" value={ area } onChange={this.handleChange} />
                         </label>
                         <label>
                             <span className={styles["check"]}>Vagas de Garagem:</span>
-                            <input type="number" placeholder="Vagas de Garagem" value={this.state.checkOut} onChange={this.handleChangeCheckOut} />
+                            <input type="number" name={ConstantesForm.Campos.vagas} placeholder="Vagas de Garagem" value={ vagas } onChange={this.handleChange} />
                         </label>
+                        {tipoImovel === ConstantesImoveis.Tipos.Apartamento && <label>
+                            <span className={styles["check"]}>Andar:</span>
+                            <input type="number" name={ConstantesForm.Campos.andar} placeholder="Andar" value={ andar } onChange={this.handleChange} />
+                        </label>}
+                        {tipoImovel === ConstantesImoveis.Tipos.Apartamento && <label>
+                            <span className={styles["check"]}>Valor do Condomínio:</span>
+                            <input type="number" name={ConstantesForm.Campos.condominio} placeholder="Condominio R$" value={ condominio } onChange={this.handleChange} />
+                        </label>}
                         <input type="checkbox" name="vehicle" value="Bike" /> I have a bike
                         <input className={styles["button"]} type="submit" value="Ver Quartos" />
                     </form>
